ba6ec95078dc4e90fe894a8b7878ae194bab4810
Sandeep Suthar <sandeepsuthar@CJ-139.local>
2023-03-30 16:51:40 +0530

    Task Manager Application in NodeJs and Javascript

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB =require('./db/connect')
+
+app.use(express.json());
+
+app.use('/api/v1/tasks',tasks)
+
+
+const port = 3000
+
+const start = async () => {
+    try {
+        await connectDB()
+        app.listen(port, console.log(`server is listening on port ${port}`))
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+start()
---
+++ db/connect.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const connectionString = 'mongodb+srv://sandeep:12345@atlascluster.51npqof.mongodb.net/Task_Manager?retryWrites=true&w=majority'
+
+
+const connectDB = (url) => {
+    return mongoose
+    .connect(connectionString,
+        {useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+        })
+    .then(()=> console.log("Connected to the DB...."))
+    .catch((err)=>console.log(err))
+}
+
+module.exports = connectDB
